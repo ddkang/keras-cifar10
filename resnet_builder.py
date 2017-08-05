2c383e19f751a7a49975df3ec7ea633e7e1d5122
Daniel Kang <daniel.d.kang@gmail.com>
2017-08-05 10:24:23 +0000

    First pass

---
+++ resnet_builder.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+import keras
+from keras.layers import Input, Dense, Activation, Flatten
+from keras.layers import Conv2D, AveragePooling2D, BatchNormalization, MaxPooling2D, ZeroPadding2D
+from keras.layers import Convolution2D
+from keras.models import Model
+from keras import layers
+from keras.utils import np_utils
+from keras import backend as K
+
+if (K.image_data_format() == 'channels_first'):
+    bn_axis = 1
+else:
+    bn_axis = 3
+
+BN_EPS = 0.00001
+
+def my_conv(inp, num_filters, kernel_size_tuple, strides=1, padding='valid', name='name'):
+    if strides == 2 and kernel_size_tuple[0] != 1:
+        x = keras.layers.convolutional.ZeroPadding2D()(inp)
+        x = Convolution2D(num_filters, kernel_size_tuple, strides=(2, 2),
+                          use_bias=False, kernel_initializer='he_normal', name=name)(x)
+    else:
+        x = Conv2D(num_filters, kernel_size_tuple, strides=strides, padding=padding,
+                   use_bias=False, kernel_initializer='he_normal', name=name)(inp)
+    return x
+
+
+def PreActBlock(stage, block, inp, numFilters, stride, isConvBlock):
+    expansion = 1
+    conv_name_base = 'conv' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = BatchNormalization(axis=bn_axis, momentum=0.1, epsilon=BN_EPS, name=bn_name_base + '2a')(inp)
+    x = Activation('relu')(x)
+
+    if isConvBlock:
+        shortcut = my_conv(x, expansion * numFilters, (1, 1),
+                           strides=stride, padding='same',
+                           name=conv_name_base + '1')
+    else:
+        shortcut = inp
+    
+    x = my_conv(x, numFilters, (3, 3), strides=stride, padding='same', name=conv_name_base + '2a')
+
+    x = BatchNormalization(axis=bn_axis, momentum=0.1, epsilon=BN_EPS, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+    x = my_conv(x, numFilters, (3, 3), padding = 'same', name=conv_name_base + '2b')
+
+    x = layers.add([x, shortcut])
+    return x
+
+def make_layer(stage, block, inp, numFilters, numBlocks, stride):
+    if stride == 1:
+        x = block(stage, 'a', inp, numFilters, stride, False)
+    else:
+        x = block(stage, 'a', inp, numFilters, stride, True)
+
+    for i in range(numBlocks - 1):
+        x = block(stage, chr(ord('b') + i), x, numFilters, 1, False)
+    return x
+
+def ResNet_builder(block, num_blocks, input_shape, num_classes):
+    img_input = Input(shape=input_shape)
+    x = my_conv(img_input, 64, (3, 3), padding='same', name='conv1')
+    x = BatchNormalization(axis = bn_axis, momentum=0.1, epsilon=BN_EPS, name='bn1')(x)
+    x = Activation('relu')(x)
+    
+    x = make_layer(1, block, x, 64, num_blocks[0], 1)
+    x = make_layer(2, block, x, 128, num_blocks[1], 2)
+    x = make_layer(3, block, x, 256, num_blocks[2], 2)
+    x = make_layer(4, block, x, 512, num_blocks[3], 2)
+    
+    x = AveragePooling2D((4, 4), strides=4)(x)
+    x = Flatten()(x)
+    x = Dense(num_classes, activation='softmax', name = 'dense')(x)
+    # x = Dense(num_classes, name = 'dense')(x)
+
+    return Model(inputs=img_input, outputs=x)
+
+   
+def ResNet18(input_shape, num_classes):
+    return ResNet_builder(PreActBlock, [2, 2, 2, 2], input_shape, num_classes)
---
+++ train.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import math
+import six
+import os
+import time
+import csv
+import h5py
+
+from collections import OrderedDict
+from collections import Iterable
+from keras.layers import Input
+from keras.models import Model
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import LearningRateScheduler
+from keras.callbacks import Callback
+from keras.datasets import cifar10
+from keras import backend as K
+
+from resnet_builder import ResNet18
+
+batch_size = 128
+num_epochs = 350
+num_classes = 10
+data_augmentation = True
+
+def step_decay(epoch):
+    initial_lrate = 0.1
+    drop = 0.1
+    epoch_drop = 100
+    if epoch < 50:
+        return initial_lrate
+    lrate = initial_lrate * math.pow(drop, math.floor((epoch - 50)/epoch_drop))
+    return lrate
+
+# Preprocess train and test set data
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+if (K.image_data_format() == 'channels_first'):
+    for i in range(x_train.shape[1]):
+        mean = np.mean(x_train[:,i,:,:])
+        std_dev = np.std(x_train[:,i,:,:])
+        x_train[:,i,:,:] -= mean
+        x_train[:,i,:,:] /= std_dev
+        x_test[:,i,:,:] -= mean
+        x_test[:,i,:,:] /= std_dev
+else:
+    for i in range(x_train.shape[3]):
+        mean = np.mean(x_train[:,:,:,i])
+        std_dev = np.std(x_train[:,:,:,i])
+        x_train[:,:,:,i] -= mean
+        x_train[:,:,:,i] /= std_dev
+        x_test[:,:,:,i] -= mean
+        x_test[:,:,:,i] /= std_dev
+
+y_train = np_utils.to_categorical(y_train, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
+
+img_input = Input(shape=x_train.shape[1:])
+
+model = ResNet18(x_train.shape[1:], num_classes=num_classes)
+
+
+datagen = ImageDataGenerator(
+            featurewise_center=False,
+            samplewise_center=False,
+            featurewise_std_normalization=False,
+            samplewise_std_normalization=False,
+            zca_whitening=False,
+            rotation_range=0,
+            width_shift_range=0.1,
+            height_shift_range=0.1,
+            fill_mode='constant',
+            cval=0,
+            horizontal_flip=True,
+            vertical_flip=False)
+datagen.fit(x_train)
+
+sgd = SGD(lr=0.01, momentum=0.9, decay=0.0, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
+                    steps_per_epoch=x_train.shape[0] // batch_size,
+                    epochs=1, validation_data=(x_test, y_test))
+for num_epochs, lr_rate in [(150, 0.1), (100, 0.01), (100, 0.001)]:
+    sgd = SGD(lr=lr_rate, momentum=0.9, decay=0.0, nesterov=False)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+    history = model.fit_generator(datagen.flow(x_train, y_train, 
+                                               batch_size=batch_size),
+                                  steps_per_epoch=x_train.shape[0] // batch_size,
+                                  epochs=num_epochs,
+                                  # callbacks=callbacks_list,
+                                  verbose=1,
+                                  validation_data=(x_test, y_test))
+
+
+if (K.image_data_format() == 'channels_first'):
+    model.save('cifar10-resnet18-pa-th.h5')
+else:
+    model.save('cifar10-resnet18-pa-tf.h5')
+
+scores = model.evaluate(x_test, y_test)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
